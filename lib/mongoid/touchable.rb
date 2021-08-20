6442ecc1af520195322e079b40f906ec75e8db73
shields <shields@tablecheck.com>
2021-08-20 14:04:55 +0900

    Gather touch updates

--- lib/mongoid/touchable.rb
+++ lib/mongoid/touchable.rb
@@ -26,8 +26,7 @@ module Mongoid
       def touch(field = nil)
         return false if _root.new_record?
 
-        touches = __gather_touch_atomic_updates(field)
-
+        touches = __gather_touch_updates(field)
         unless touches.blank?
           selector = _root.atomic_selector
           _root.collection.find(selector).update_one(positionally(selector, '$set' => touches), session: _session)
@@ -37,14 +36,14 @@ module Mongoid
         true
       end
 
-      def __gather_touch_atomic_updates(field = nil)
+      def __gather_touch_updates(field = nil)
         current = Time.now
         field = database_field_name(field)
         write_attribute(:updated_at, current) if respond_to?("updated_at=")
         write_attribute(field, current) if field
 
         touches = __touch_atomic_sets(field) || {}
-        touches.merge!(_parent.__gather_touch_atomic_updates || {}) if _parent
+        touches.merge!(_parent.__gather_touch_updates || {}) if _parent
         touches
       end
 
